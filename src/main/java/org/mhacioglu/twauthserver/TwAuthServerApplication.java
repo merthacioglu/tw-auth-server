27b727acb25595419d062a0f3d0fcb135f85f39b
Mert Hacioglu <mhacioglu96@gmail.com>
2024-09-18 00:17:21 +0100

    Initial commit

---
+++ src/main/java/org/mhacioglu/twauthserver/TwAuthServerApplication.java
@@ -0,0 +1,13 @@
+package org.mhacioglu.twauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwAuthServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/mhacioglu/twauthserver/TwAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mhacioglu.twauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwAuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
